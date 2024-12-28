48de96d0711ecffb98be116f08c1bb8458198bd0
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-28 21:58:57 +0000

    Handling POST Request

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -4,3 +4,17 @@ export async function GET() {
     return Response.json(comments);
 }
 
+export async function POST(request: Request) {
+    const comment = await request.json();
+    const newcomment = {
+        id: comments.length + 1,
+        text: comment.text
+    };
+    comments.push(newcomment);
+    return new Response(JSON.stringify(newcomment), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        status: 201,
+    })
+}
