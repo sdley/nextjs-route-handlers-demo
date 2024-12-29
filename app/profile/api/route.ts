ce64317e6620617e57b9975e63a8fb253553f5fb
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-29 00:31:02 +0000

    Headers in Route Handlers

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,3 +1,15 @@
-export async function GET() {
-    return new Response("Profile API data");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers();
+
+    console.log(requestHeaders.get("Authorization"));
+    console.log((await headerList).get("Authorization"));
+    return new Response("<h1>Profile API data</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+        },
+    });
 }
